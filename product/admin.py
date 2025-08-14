6ee8ad035725e1e460aa64e3d055af0a3182dc6b
Ratish149 <ratish.shakya149@gmail.com>
2025-08-14 15:16:06 +0545

    dfd

--- product/admin.py
+++ product/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from .models import Product
-from unfold.admin import ModelAdmin
-
 # Register your models here.
 
 
 @admin.register(Product)
-class ProductAdmin(ModelAdmin):
+class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'price', 'stock')
     search_fields = ('name', 'description')
     list_filter = ('stock',)
