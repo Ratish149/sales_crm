e07c12b60b8a013ec261f16045d6ac8a731a93f8
Ratish149 <ratish.shakya149@gmail.com>
2025-08-15 15:04:11 +0545

    dfd

--- order/models.py
+++ order/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from product.models import Product
 # Create your models here.
 
 
@@ -21,3 +21,6 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.quantity}"
