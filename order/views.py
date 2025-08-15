5b66571d93bad4d4d8023fbf5b720606e643cf74
Ratish149 <ratish.shakya149@gmail.com>
2025-08-15 16:03:22 +0545

    dfd

--- order/views.py
+++ order/views.py
@@ -13,7 +13,7 @@ class CustomPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
-s
+
 
 class OrderListCreateAPIView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
