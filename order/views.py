9e16c7d816fabad9ab3d92395b8859c813092e3f
Ratish149 <ratish.shakya149@gmail.com>
2025-08-15 16:01:18 +0545

    dfd

--- order/views.py
+++ order/views.py
@@ -4,12 +4,22 @@ from .models import Order
 from .serializers import OrderSerializer
 
 # List and Create Orders
+from rest_framework.pagination import PageNumberPagination
 
+# Create your views here.
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+s
 
 class OrderListCreateAPIView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = CustomPagination
 
 # Retrieve, Update, Delete single Order
 
