82e650f69d56f79682c0bc05539662a7a9e2d852
Ratish149 <ratish.shakya149@gmail.com>
2025-09-01 11:05:58 +0545

    dfd

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,9 @@ class Blog(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        unique_together = ('title', 'slug')
+
     def __str__(self):
         return self.title
 
