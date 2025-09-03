1e6050065befcced7f569c9291c3a6d681f8e54b
Ratish149 <ratish.shakya149@gmail.com>
2025-09-03 17:06:55 +0545

    dfd

--- advertisement/views.py
+++ advertisement/views.py
@@ -3,11 +3,19 @@ from .models import PopUp, PopUpForm
 from .serializers import PopUpSerializer, PopUpFormSerializer
 
 # Create your views here.
+from rest_framework.pagination import PageNumberPagination
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 
 class PopUpCreateView(generics.ListCreateAPIView):
     queryset = PopUp.objects.all()
     serializer_class = PopUpSerializer
+    pagination_class = CustomPagination
 
 
 class PopUpRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
--- contact/views.py
+++ contact/views.py
@@ -3,11 +3,19 @@ from .models import Contact
 from .serializers import ContactSerializer
 
 # Create your views here.
+from rest_framework.pagination import PageNumberPagination
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 
 class ContactCreateView(generics.ListCreateAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+    pagination_class = CustomPagination
 
 
 class ContactRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
