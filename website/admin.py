38aaf3305faaaa1be3bed61369f05b75d14cace1
Ratish149 <ratish.shakya149@gmail.com>
2025-09-16 11:16:58 +0545

    dfd

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Page, PageComponent
+from .models import Page, PageComponent, Theme
 
 admin.site.register(Page)
 admin.site.register(PageComponent)
+admin.site.register(Theme)
