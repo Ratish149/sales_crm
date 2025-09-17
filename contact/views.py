b6d1b19b75da20fec7b0111cf4bbad70285896fc
Ratish149 <ratish.shakya149@gmail.com>
2025-09-17 11:03:02 +0545

    fgf

--- contact/views.py
+++ contact/views.py
@@ -1,12 +1,12 @@
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.exceptions import ValidationError
 from rest_framework import generics
 from .models import Contact, NewsLetter
 from .serializers import ContactSerializer, NewsLetterSerializer
-
-# Create your views here.
 from rest_framework.pagination import PageNumberPagination
+from sales_crm.utils.error_handler import (
+    duplicate_entry,
+    ErrorMessage,
+    handle_transaction_errors
+)
 
 
 class CustomPagination(PageNumberPagination):
@@ -31,11 +31,15 @@ class NewsLetterCreateView(generics.ListCreateAPIView):
     serializer_class = NewsLetterSerializer
     pagination_class = CustomPagination
 
+    @handle_transaction_errors
     def create(self, request, *args, **kwargs):
         email = request.data.get('email')
-        if email and NewsLetter.objects.filter(email=email, is_subscribed=True).exists():
-            raise ValidationError(
-                {'email': 'This email is already subscribed to the newsletter.'})
+        if email and NewsLetter.objects.filter(email__iexact=email, is_subscribed=True).exists():
+            return duplicate_entry(
+                message=ErrorMessage.DUPLICATE_ENTRY,
+                params={
+                    'email': 'This email is already subscribed to the newsletter.'}
+            )
         return super().create(request, *args, **kwargs)
 
 
