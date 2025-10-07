b51a6c7707686e098d4aaec7ee692fc5807b6111
Ratish149 <ratish.shakya149@gmail.com>
2025-10-07 11:47:51 +0545

    dfd

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -10,15 +10,32 @@ from .views import (
 )
 
 urlpatterns = [
-    path("portfolio/category/", PortfolioCategoryListCreateAPIView.as_view()),
+    path(
+        "portfolio/category/",
+        PortfolioCategoryListCreateAPIView.as_view(),
+        name="portfolio_category_list_create",
+    ),
     path(
         "portfolio/category/<int:pk>/",
         PortfolioCategoryRetrieveUpdateDestroyAPIView.as_view(),
+        name="portfolio_category_retrieve_update_destroy",
     ),
-    path("portfolio/", PortfolioListCreateAPIView.as_view()),
-    path("portfolio/<slug:slug>/", PortfolioRetrieveUpdateDestroyAPIView.as_view()),
-    path("portfolio/tags/", PortfolioTagsListCreateAPIView.as_view()),
     path(
-        "portfolio/tags/<int:pk>/", PortfolioTagsRetrieveUpdateDestroyAPIView.as_view()
+        "portfolio/", PortfolioListCreateAPIView.as_view(), name="portfolio_list_create"
+    ),
+    path(
+        "portfolio/<slug:slug>/",
+        PortfolioRetrieveUpdateDestroyAPIView.as_view(),
+        name="portfolio_retrieve_update_destroy",
+    ),
+    path(
+        "portfolio/tags/",
+        PortfolioTagsListCreateAPIView.as_view(),
+        name="portfolio_tags_list_create",
+    ),
+    path(
+        "portfolio/tags/<int:pk>/",
+        PortfolioTagsRetrieveUpdateDestroyAPIView.as_view(),
+        name="portfolio_tags_retrieve_update_destroy",
     ),
 ]
