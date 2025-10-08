d05a9026ecfa948de9568dd66a5ea5f49df349da
Ratish149 <ratish.shakya149@gmail.com>
2025-10-08 17:03:50 +0545

    dfd

--- service/views.py
+++ service/views.py
@@ -1,14 +1,22 @@
 from rest_framework import generics
 
+# Create your views here.
+from rest_framework.pagination import PageNumberPagination
+
 from .models import Service
 from .serializers import ServiceSerializer
 
-# Create your views here.
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
 
 
 class ServiceListCreateView(generics.ListCreateAPIView):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
+    pagination_class = CustomPagination
 
 
 class ServiceRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
