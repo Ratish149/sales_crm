974a4e28a9c8adca1016b5ed057b2299b12f67c4
Ratish149 <ratish.shakya149@gmail.com>
2025-10-12 17:09:13 +0545

    setting change

--- sales_crm/settings.py
+++ sales_crm/settings.py
@@ -142,7 +142,7 @@ WSGI_APPLICATION = "sales_crm.wsgi.application"
 #     }
 # }
 
-DATABASES = {
+""" DATABASES = {
     "default": {
         "ENGINE": "django_tenants.postgresql_backend",  # required for django-tenants
         "NAME": "builder",
@@ -154,9 +154,9 @@ DATABASES = {
             "options": "-c search_path=public"  # important for first migration
         },
     }
-}
+} """
 
-""" DATABASES = {
+DATABASES = {
     "default": {
         "ENGINE": "django_tenants.postgresql_backend",
         "NAME": os.getenv("DB_NAME"),
@@ -166,7 +166,7 @@ DATABASES = {
         "PORT": "",
         "OPTIONS": {"options": "-c search_path=public"},
     }
-} """
+}
 
 
 DATABASE_ROUTERS = ("django_tenants.routers.TenantSyncRouter",)
