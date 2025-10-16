92655da1f97d4d6ad5d9921bef73c3cdcf2e8882
Ratish149 <ratish.shakya149@gmail.com>
2025-10-16 12:22:57 +0545

    dss

--- payment_gateway/views.py
+++ payment_gateway/views.py
@@ -27,7 +27,31 @@ class PaymentListCreateAPIView(generics.ListCreateAPIView):
             return PaymentSmallSerializer
         return PaymentSerializer
 
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        if response.status_code == 201:
+            instance_data = response.data
+            custom_response = {
+                'id': instance_data.get('id'),
+                'payment_type': instance_data.get('payment_type'),
+                'merchant_code': instance_data.get('merchant_code')
+            }
+            response.data = custom_response
+        return response
+
 
 class PaymentRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        if response.status_code == 200:
+            instance_data = response.data
+            custom_response = {
+                'id': instance_data.get('id'),
+                'payment_type': instance_data.get('payment_type'),
+                'merchant_code': instance_data.get('merchant_code')
+            }
+            response.data = custom_response
+        return response
