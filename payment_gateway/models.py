8b9f5b96e80d15f0230d0fee6d7ab68d39a47298
Ratish149 <ratish.shakya149@gmail.com>
2025-10-16 12:30:02 +0545

    dfd

--- payment_gateway/models.py
+++ payment_gateway/models.py
@@ -14,6 +14,3 @@ class Payment(models.Model):
     secret_key = models.CharField(max_length=255, null=True, blank=True)
     merchant_code = models.CharField(max_length=255, null=True, blank=True)
     is_enabled = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.payment_type
