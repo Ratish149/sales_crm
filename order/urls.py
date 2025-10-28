e7076b0be98350a70b3237ac4e117c5b113579cf
Ratish149 <ratish.shakya149@gmail.com>
2025-10-28 15:21:09 +0545

    Test auto deploy

--- order/urls.py
+++ order/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
-from .views import OrderListCreateAPIView, OrderRetrieveUpdateDestroyAPIView, DashboardStatsView, MyOrderListAPIView, MyOrderStatusView
+
+from .views import (
+    DashboardStatsView,
+    MyOrderListAPIView,
+    MyOrderStatusView,
+    OrderListCreateAPIView,
+    OrderRetrieveUpdateDestroyAPIView,
+)
 
 urlpatterns = [
-    path('order/', OrderListCreateAPIView.as_view(), name='order-list-create'),
-    path('order/<int:pk>/', OrderRetrieveUpdateDestroyAPIView.as_view(),
-         name='order-retrieve-update-destroy'),
-    path('dashboard-stats/', DashboardStatsView.as_view(), name='dashboard-stats'),
-    path('my-order/', MyOrderListAPIView.as_view(), name='customer-order'),
-    path('my-order-status/', MyOrderStatusView.as_view(),
-         name='customer-order-status'),
+    path("order/", OrderListCreateAPIView.as_view(), name="order-list-create"),
+    path(
+        "order/<int:pk>/",
+        OrderRetrieveUpdateDestroyAPIView.as_view(),
+        name="order-retrieve-update-destroy",
+    ),
+    path("dashboard-stats/", DashboardStatsView.as_view(), name="dashboard-stats"),
+    path("my-order-list/", MyOrderListAPIView.as_view(), name="customer-order"),
+    path("my-order-status/", MyOrderStatusView.as_view(), name="customer-order-status"),
 ]
