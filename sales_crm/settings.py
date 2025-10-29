8438babd5408ea99b040af464514fad9fd6c5e79
Ratish149 <ratish.shakya149@gmail.com>
2025-10-29 11:47:31 +0545

    change setting

--- sales_crm/settings.py
+++ sales_crm/settings.py
@@ -181,6 +181,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://127.0.0.1:8001",
     "http://localhost:3000",
+    "http://localhost:8081",
     "https://www.nepdora.com",
     "https://browser-classic-kind-bracelets.trycloudflare.com",
 ]
@@ -188,6 +189,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://127.0.0.1:8001",
     "http://localhost:3000",
+    "http://localhost:8081",
     "https://www.nepdora.com",
     "https://browser-classic-kind-bracelets.trycloudflare.com",
 ]
