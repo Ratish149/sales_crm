9722fcc367a9d1a7d0cc6043f648408fefa5004b
Ratish149 <ratish.shakya149@gmail.com>
2025-11-02 17:26:23 +0545

    dffd

--- facebook/urls.py
+++ facebook/urls.py
@@ -5,7 +5,7 @@ from .views import FacebookListCreateView, FacebookRetrieveUpdateDestroyView
 urlpatterns = [
     path("facebook/", FacebookListCreateView.as_view(), name="facebook-list-create"),
     path(
-        "facebook/<int:pk>",
+        "facebook/<int:pk>/",
         FacebookRetrieveUpdateDestroyView.as_view(),
         name="facebook-retrieve-update-destroy",
     ),
