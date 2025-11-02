f71ce7968780bdb8f2a1576185b9ea5db09758ab
Ratish149 <ratish.shakya149@gmail.com>
2025-11-02 10:37:48 +0545

    fix model

--- google_analytic/models.py
+++ google_analytic/models.py
@@ -5,7 +5,7 @@ from solo.models import SingletonModel
 # Create your models here.
 class GoogleAnalytic(SingletonModel):
     measurement_id = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=False)
+    is_enabled = models.BooleanField(default=False)
 
     def __str__(self):
         return "Google Analytic"
