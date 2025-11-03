08681f4fa025ada80b6215b81e7bdf4207e53605
Ratish149 <ratish.shakya149@gmail.com>
2025-11-03 10:33:30 +0545

    added permission to facebook

--- facebook/views.py
+++ facebook/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Facebook
 from .serializers import FacebookSerializer
@@ -7,8 +8,10 @@ from .serializers import FacebookSerializer
 class FacebookListCreateView(generics.ListCreateAPIView):
     queryset = Facebook.objects.filter(is_enabled=True)
     serializer_class = FacebookSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class FacebookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Facebook.objects.all()
     serializer_class = FacebookSerializer
+    permission_classes = [IsAuthenticated]
