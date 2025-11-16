872cb712aa39b6e86eb8724365683a589f1927c9
Ratish149 <ratish.shakya149@gmail.com>
2025-11-16 12:23:00 +0545

    template page model remove

--- template/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Template, TemplatePage, TemplatePageComponent
-
-# Register your models here.
-admin.site.register(Template)
-admin.site.register(TemplatePage)
-admin.site.register(TemplatePageComponent)
