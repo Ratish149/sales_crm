0e57e1b027212344cefe8f1bb0606dec1677c43a
Ratish149 <ratish.shakya149@gmail.com>
2025-11-23 13:16:40 +0545

    no dublicate blog with same title

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,37 +1,65 @@
 from rest_framework import serializers
+
 from .models import Blog, Tags
 
 
 class TagsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tags
-        fields = '__all__'
+        fields = "__all__"
 
 
 class TagsSmallSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tags
-        fields = ['id', 'name', 'slug']
+        fields = ["id", "name", "slug"]
 
 
 class BlogSerializer(serializers.ModelSerializer):
     tags = TagsSmallSerializer(many=True, read_only=True)
-    tag_ids = serializers.ListField(child=serializers.IntegerField(
-    ), write_only=True, required=False, source='tags')
+    tag_ids = serializers.ListField(
+        child=serializers.IntegerField(), write_only=True, required=False, source="tags"
+    )
 
     class Meta:
         model = Blog
-        fields = ['id', 'title', 'slug', 'content', 'tags',
-                  'thumbnail_image', 'thumbnail_image_alt_description', 'time_to_read', 'tag_ids', 'meta_title', 'meta_description', 'created_at', 'updated_at',]
+        fields = [
+            "id",
+            "title",
+            "slug",
+            "content",
+            "tags",
+            "thumbnail_image",
+            "thumbnail_image_alt_description",
+            "time_to_read",
+            "tag_ids",
+            "meta_title",
+            "meta_description",
+            "created_at",
+            "updated_at",
+        ]
+
+    def validate_title(self, value):
+        if self.instance:
+            if Blog.objects.filter(title=value).exclude(id=self.instance.id).exists():
+                raise serializers.ValidationError(
+                    "Blog with this title already exists."
+                )
+        else:
+            if Blog.objects.filter(title=value).exists():
+                raise serializers.ValidationError(
+                    "Blog with this title already exists."
+                )
+        return value
 
     def create(self, validated_data):
-        tag_ids = validated_data.pop('tags', [])
+        tag_ids = validated_data.pop("tags", [])
         blog = Blog.objects.create(**validated_data)
         blog.tags.set(tag_ids)
         return blog
 
     def update(self, instance, validated_data):
-        tag_ids = validated_data.pop('tags', [])
+        tag_ids = validated_data.pop("tags", [])
         instance = super().update(instance, validated_data)
         instance.tags.set(tag_ids)
         return instance
