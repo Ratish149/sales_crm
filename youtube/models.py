79a34aefb1e7b071ae4e7d9f7c9cd3a16fe312c7
Ratish149 <ratish.shakya149@gmail.com>
2025-11-27 14:14:20 +0545

    remove youtube model to replace it with videos model

--- youtube/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Youtube(models.Model):
-    url = models.URLField(unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
