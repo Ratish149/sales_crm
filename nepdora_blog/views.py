26f1868fc421da280374d777922f4cdce3f60a3d
Ratish149 <ratish.shakya149@gmail.com>
2026-01-16 15:38:19 +0545

    blog fix

--- nepdora_blog/views.py
+++ nepdora_blog/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django_filters import rest_framework as django_filters
-from rest_framework import filters, generics
+from rest_framework import filters, generics, permissions
 from rest_framework.pagination import PageNumberPagination
 
 from .models import Blog, BlogCategory, Tags
@@ -16,12 +16,16 @@ class CustomPagination(PageNumberPagination):
 class BlogCategoryListCreateView(generics.ListCreateAPIView):
     queryset = BlogCategory.objects.all()
     serializer_class = BlogCategorySerializer
+    permission_classes = [permissions.AllowAny]
+    authentication_classes = []
 
 
 class BlogCategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = BlogCategory.objects.all()
     serializer_class = BlogCategorySerializer
     lookup_field = "slug"
+    permission_classes = [permissions.AllowAny]
+    authentication_classes = []
 
 
 class BlogFilterSet(django_filters.FilterSet):
@@ -42,27 +46,37 @@ class BlogListCreateView(generics.ListCreateAPIView):
     filter_backends = [django_filters.DjangoFilterBackend, filters.SearchFilter]
     filterset_class = BlogFilterSet
     search_fields = ["title"]
+    permission_classes = [permissions.AllowAny]
+    authentication_classes = []
 
 
 class BlogRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
     lookup_field = "slug"
+    permission_classes = [permissions.AllowAny]
+    authentication_classes = []
 
 
 class TagsListCreateView(generics.ListCreateAPIView):
     queryset = Tags.objects.all()
     serializer_class = TagsSerializer
     pagination_class = CustomPagination
+    permission_classes = [permissions.AllowAny]
+    authentication_classes = []
 
 
 class TagsRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Tags.objects.all()
     serializer_class = TagsSerializer
     lookup_field = "slug"
+    permission_classes = [permissions.AllowAny]
+    authentication_classes = []
 
 
 # Get recent blogs api
 class RecentBlogsView(generics.ListAPIView):
     queryset = Blog.objects.all().order_by("-created_at")[:5]
     serializer_class = BlogSerializer
+    permission_classes = [permissions.AllowAny]
+    authentication_classes = []
