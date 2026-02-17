74714edcbe1ae654ac46aca24845a9dfedcb90bf
Ratish149 <ratish.shakya149@gmail.com>
2026-02-17 15:06:30 +0545

    fgf

--- sales_crm/asgi.py
+++ sales_crm/asgi.py
@@ -1,18 +1,22 @@
 import os
 
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sales_crm.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry is populated
+# before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 
 import builder.routing
 import facebook.routing
 import website.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sales_crm.settings")
-
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(
             URLRouter(
                 facebook.routing.websocket_urlpatterns
