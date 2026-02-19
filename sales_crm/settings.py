78164f0a3d93097218cdb235997e6dfb1bbc3c97
Ratish149 <ratish.shakya149@gmail.com>
2026-02-19 11:17:26 +0545

    dfd

--- sales_crm/settings.py
+++ sales_crm/settings.py
@@ -186,7 +186,7 @@ CACHES = {
 #     }
 # }
 
-DATABASES = {
+""" DATABASES = {
     "default": {
         "ENGINE": "django_tenants.postgresql_backend",  # required for django-tenants
         "NAME": "builder",
@@ -198,9 +198,9 @@ DATABASES = {
             "options": "-c search_path=public"  # important for first migration
         },
     }
-}
+} """
 
-""" DATABASES = {
+DATABASES = {
     "default": {
         "ENGINE": "django_tenants.postgresql_backend",
         "NAME": os.getenv("DB_NAME"),
@@ -210,7 +210,7 @@ DATABASES = {
         "PORT": "",
         "OPTIONS": {"options": "-c search_path=public"},
     }
-} """
+}
 
 DATABASE_ROUTERS = ("django_tenants.routers.TenantSyncRouter",)
 
