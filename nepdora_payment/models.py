a687774abbe67ea896054380558b3150591d62ec
Ratish149 <ratish.shakya149@gmail.com>
2026-03-13 17:16:14 +0545

    fix model

--- nepdora_payment/models.py
+++ nepdora_payment/models.py
@@ -46,7 +46,7 @@ class TenantCentralPaymentHistory(models.Model):
     payment_type = models.CharField(max_length=10, choices=PAYMENT_CHOICES)
     pay_amount = models.DecimalField(max_digits=10, decimal_places=2)
     transaction_id = models.CharField(max_length=255)
-    products_purchased = models.JSONField(default=dict, blank=True, null=True)
+    products_purchased = models.JSONField(default=list, blank=True, null=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
     additional_info = models.JSONField(default=dict, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- payment_gateway/models.py
+++ payment_gateway/models.py
@@ -30,11 +30,11 @@ class PaymentHistory(models.Model):
     payment_type = models.CharField(max_length=10, choices=PAYMENT_CHOICES)
     pay_amount = models.DecimalField(max_digits=10, decimal_places=2)
     transaction_id = models.CharField(max_length=255)
-    products_purchased = models.JSONField(default=dict, blank=True, null=True)
+    products_purchased = models.JSONField(default=list, blank=True, null=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
     additional_info = models.JSONField(default=dict, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.tenant.name} - {self.payment_type} - {self.pay_amount} ({self.status})"
+        return f"{self.payment_type} - {self.pay_amount} ({self.status})"
