9f0236742281156f81384d6bc5bc97d0f0b22ab8
Ratish149 <ratish.shakya149@gmail.com>
2026-03-13 16:00:07 +0545

    dfd

--- nepdora_payment/serializers.py
+++ nepdora_payment/serializers.py
@@ -1,20 +1,40 @@
 from rest_framework import serializers
+
+from tenants.models import Client
+
 from .models import NepdoraPayment, TenantCentralPaymentHistory, TenantTransferHistory
 
 
 class NepdoraPaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = NepdoraPayment
-        fields = '__all__'
+        fields = "__all__"
 
 
 class TenantCentralPaymentHistorySerializer(serializers.ModelSerializer):
+    tenant = serializers.CharField()
+
     class Meta:
         model = TenantCentralPaymentHistory
-        fields = '__all__'
+        fields = "__all__"
+
+    def create(self, validated_data):
+        tenant_name = validated_data.pop("tenant")
+        try:
+            tenant = Client.objects.get(name=tenant_name)
+        except Client.DoesNotExist:
+            raise serializers.ValidationError(
+                {"tenant": f"Tenant with name '{tenant_name}' does not exist."}
+            )
+        except Client.MultipleObjectsReturned:
+            raise serializers.ValidationError(
+                {"tenant": f"Multiple tenants found with name '{tenant_name}'."}
+            )
+
+        return TenantCentralPaymentHistory.objects.create(tenant=tenant, **validated_data)
 
 
 class TenantTransferHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = TenantTransferHistory
-        fields = '__all__'
+        fields = "__all__"
