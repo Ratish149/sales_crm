bc0cf97a24eed1d74b930bf1f6dc13b17cd595de
Ratish149 <ratish.shakya149@gmail.com>
2026-03-23 15:22:18 +0545

    added created at date in domain model

--- tenants/models.py
+++ tenants/models.py
@@ -111,7 +111,8 @@ class Client(TenantMixin):
 
 
 class Domain(DomainMixin):
-    pass
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class FacebookPageTenantMap(models.Model):
