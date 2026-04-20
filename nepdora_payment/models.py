67a1f23f08b73cb72fcfc65ecbdce0f279cc8100
Ratish149 <ratish.shakya149@gmail.com>
2026-04-20 14:58:46 +0545

    fix

--- nepdora_payment/models.py
+++ nepdora_payment/models.py
@@ -89,7 +89,9 @@ class SMSPurchaseHistory(models.Model):
     )
     amount = models.IntegerField(help_text="Number of credits purchased")
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    transaction_id = models.CharField(max_length=255, unique=True)
+    transaction_id = models.CharField(
+        max_length=255, null=True, blank=True, unique=True
+    )
     purchased_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
