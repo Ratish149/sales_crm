f8bdeaa5dadb3605cd79c869b64f7b1e26b500d5
Ratish149 <ratish.shakya149@gmail.com>
2026-05-03 11:18:40 +0545

    dfd

--- sales_crm/settings.py
+++ sales_crm/settings.py
@@ -229,7 +229,7 @@ DATABASES = {
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": "127.0.0.1",
         "PORT": "6432",
-        "OPTIONS": {"options": "-c search_path=public"},
+        "OPTIONS": {},
         "CONN_MAX_AGE": 0,
     }
 }
