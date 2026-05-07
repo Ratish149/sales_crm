5902fa8a4f3d5d9ea452a923289f693ab55466bf
Ratish149 <ratish.shakya149@gmail.com>
2026-05-07 14:49:47 +0545

    dfd

--- migrate_schema.py
+++ migrate_schema.py
@@ -1,12 +1,18 @@
-# migrate_batch.py
+import os
+
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sales_crm.settings")
+django.setup()
+
 from django.core.management import call_command
 
 from tenants.models import Client
 
 schemas = Client.objects.values_list("schema_name", flat=True)
 
-for i, schema in enumerate(schemas):
-    print(f"[{i}] Migrating {schema}")
+for schema in schemas:
+    print("Migrating:", schema)
     try:
         call_command("migrate_schemas", schema_name=schema)
     except Exception as e:
