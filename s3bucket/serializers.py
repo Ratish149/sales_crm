205fabc3763745c0d14e2807ce40c54e8e84fa42
Ratish149 <ratish.shakya149@gmail.com>
2026-05-17 13:37:41 +0545

    feat: add S3BulkUploadView and MultipleFileUploadSerializer to support bulk file uploads

--- s3bucket/serializers.py
+++ s3bucket/serializers.py
@@ -4,10 +4,19 @@ from rest_framework import serializers
 class FileUploadSerializer(serializers.Serializer):
     file = serializers.FileField()
 
+
+class MultipleFileUploadSerializer(serializers.Serializer):
+    files = serializers.ListField(
+        child=serializers.FileField(allow_empty_file=False), allow_empty=False
+    )
+
+
 class FileDeleteSerializer(serializers.Serializer):
     urls = serializers.ListField(child=serializers.URLField(), required=False)
 
     def validate(self, data):
-        if not any([data.get('urls')]):
-            raise serializers.ValidationError("At least one of 'urls' must be provided.")
+        if not any([data.get("urls")]):
+            raise serializers.ValidationError(
+                "At least one of 'urls' must be provided."
+            )
         return data
--- s3bucket/urls.py
+++ s3bucket/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import S3UploadView, S3DeleteView, S3ListView, S3DeleteFolderView
+
+from .views import (
+    S3BulkUploadView,
+    S3DeleteFolderView,
+    S3DeleteView,
+    S3ListView,
+    S3UploadView,
+)
 
 urlpatterns = [
-    path('upload/', S3UploadView.as_view(), name='s3-upload'),
-    path('delete/', S3DeleteView.as_view(), name='s3-delete'),
-    path('files/', S3ListView.as_view(), name='s3-list'),
-    path('delete-folder/', S3DeleteFolderView.as_view(), name='s3-delete-folder'),
+    path("upload/", S3UploadView.as_view(), name="s3-upload"),
+    path("bulk-upload/", S3BulkUploadView.as_view(), name="s3-bulk-upload"),
+    path("delete/", S3DeleteView.as_view(), name="s3-delete"),
+    path("files/", S3ListView.as_view(), name="s3-list"),
+    path("delete-folder/", S3DeleteFolderView.as_view(), name="s3-delete-folder"),
 ]
