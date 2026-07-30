a75c7550f3106078175caebd57938bf5831c3129
Ratish149 <ratish.shakya149@gmail.com>
2026-07-30 14:37:19 +0545

    feat: register BlogCategory, Tags, and Blog models with customized admin views

--- blog/admin.py
+++ blog/admin.py
@@ -1,17 +1,40 @@
 from django.contrib import admin
 from tinymce.widgets import TinyMCE
 
-from .models import Blog, Tags
-
-# Register your models here.
-admin.site.register(Tags)
+from .models import Blog, BlogCategory, Tags
 
 
+@admin.register(BlogCategory)
+class BlogCategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug", "created_at", "updated_at")
+    search_fields = ("name", "slug")
+    ordering = ("name",)
+
+
+@admin.register(Tags)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug", "created_at", "updated_at")
+    search_fields = ("name", "slug")
+    ordering = ("name",)
+
+
+@admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "category",
+        "author",
+        "is_featured",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("is_featured", "category", "created_at")
+    search_fields = ("title", "author", "content")
+    raw_id_fields = ("category",)
+    filter_horizontal = ("tags",)
+
     def formfield_for_dbfield(self, db_field, **kwargs):
         if db_field.name == "content":
             return db_field.formfield(widget=TinyMCE())
         return super().formfield_for_dbfield(db_field, **kwargs)
-
-
-admin.site.register(Blog, BlogAdmin)
