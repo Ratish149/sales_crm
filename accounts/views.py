45b14d1df5813ccf3ba040711763c4317923fbb7
Ratish149 <ratish.shakya149@gmail.com>
2026-08-16 16:20:38 +0545

    refactor: remove initialization of SiteConfig during tenant schema creation

--- accounts/views.py
+++ accounts/views.py
@@ -180,13 +180,9 @@ class CustomSignupView(APIView):
 
                     # Initialise singletons for the new tenant schema
                     from sms.models import SMSSetting  # noqa: PLC0415
-                    from website.models import SiteConfig  # noqa: PLC0415
 
                     with schema_context(storeName):
                         SMSSetting.objects.get_or_create(pk=1)
-                        site_config = SiteConfig.get_solo()
-                        site_config.enable_pasalbiz = False
-                        site_config.save()
 
                     # For template accounts, assign a premium plan with no expiration
                     if is_template_account:
